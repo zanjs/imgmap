39b9fe0f200f1707f10a3e4e68817ce605022055
zanjs <root@zanjs.com>
2016-05-30 17:46:17 +0800

    新增链接 id 的替换 :sweat_smile:

--- .tmp/js/app.js
+++ .tmp/js/app.js
@@ -9603,14 +9603,14 @@ function getTitleFrom() {
     
     var pagei = PL.open({
     type: 1, //1代表页面层
-    content: '<input type="text"  id="edmName"  placeholder="请输入 EDM 名称" ><a id="okName">确认</a>',
-    style: 'width:300px; height:90px; border:none;',
+    content: '<input type="text"  id="edmName"  placeholder="请输入 EDM 名称" ><input type="text"  id="edmId"  placeholder="请输入 EDM ID" ><a id="okName">确认</a>',
+    style: 'width:300px; height:120px; border:none;',
     success: function(oPan){
        D("#edmName").focus();
        
         D("#okName").on("click",function () {
                     
-                ajaxCreate(D("#edmName").val())
+                ajaxCreate(D("#edmName").val(),D("#edmId").val())
                 
                 PL.close(pagei)
         })
@@ -9620,15 +9620,15 @@ function getTitleFrom() {
 }
 
 
-function ajaxCreate(title) {
+function ajaxCreate(title,id) {
     
     var str =  D("#codeD").text();
     
     
     D.ajax({
             type: "POST",
-            url: "p.php",
-            data: "t="+ title +"&i="+str,
+            url: "p2.php",
+            data: "t="+ title +"&i="+str+"&id="+id,
             success: function(msg){
                 
                 PL.open({
